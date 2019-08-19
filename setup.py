0e4e7d4673ce11e2fed4886317005023db71be07
Timothy Moore <mtimothy984@gmail.com>
2019-08-19 08:03:58 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""Uses setuptools to install the ignite_simple module"""
+import setuptools
+import os
+
+setuptools.setup(
+    name='ignite_simple',
+    version='0.0.1',
+    author='Timothy Moore',
+    author_email='mtimothy984@gmail.com',
+    description='Easily train pytorch models with automatic LR and BS tuning',
+    license='CC0',
+    keywords='torch pytorch models machine-learning learning-rate batch-size hyperparameters',
+    url='https://github.com/tjstretchalot/ignite-simple',
+    packages=['ignite_simple'],
+    long_description=open(
+        os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    install_requires=['pca3dvis', 'torch', 'numpy', 'matplotlib', 'scipy',
+                      'pyzmq', 'beautifulsoup4', 'pytorch-ignite'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Topic :: Utilities'),
+    python_requires='>=3.6',
+)
